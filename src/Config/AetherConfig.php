727c7d6ec958be9ef696338cd5051bc529649047
corgab <corbanigabriele@gmail.com>
2026-09-10 13:15:57 +0200

    Apply review feedback: mark Python configuration methods as deprecated

--- src/Config/AetherConfig.php
+++ src/Config/AetherConfig.php
@@ -49,6 +49,8 @@ final class AetherConfig
 
     /**
      * Python executable used to run the bin/python scripts (`aether.python_path`).
+     *
+     * @deprecated Python support will be removed in the 100% PHP rewrite.
      */
     public function pythonPath(): string
     {
@@ -57,6 +59,8 @@ final class AetherConfig
 
     /**
      * Seconds a Python subprocess may run before it is killed (`aether.process_timeout`).
+     *
+     * @deprecated Python support will be removed in the 100% PHP rewrite.
      */
     public function processTimeout(): int
     {
